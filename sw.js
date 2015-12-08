7162b3bae11bca19783297148e50dd44bf2ca713
Jordan Adler <jmadler@google.com>
2015-12-08 11:34:33 -0800

    initial commit

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+  appshell: 'fortunate-appshell-v' + CACHE_VERSION 
+};
+
+self.addEventListener('install', function(event) {
+  var app_shell = [
+    'index.html',
+    'fortunate.js',
+    'fortunes/basic.txt'
+  ];
+
+  event.waitUntil(
+    caches.open('fortunate-appshell-v1').then(function(cache) {
+      return cache.addAll(app_shell);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true}).then(function(response) {
+        return response || fetch(event.request);
+    })
+  );
+});
